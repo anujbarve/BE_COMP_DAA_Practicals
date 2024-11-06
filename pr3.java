534597be37d6010a25e7f4530db7911806ff5bb5
Anuj Barve <anujbarve27@gmail.com>
2024-11-06 19:15:25 +0530

    simplified fractional knapsack code

--- pr3.java
+++ pr3.java
@@ -1,63 +1,52 @@
 import java.util.*;
 
-class Item {
-    int profit, weight;
+class Item{
+    int profit;
+    int weight;
 
-    public Item(int profit, int weight) {
-        this.profit = profit;
-        this.weight = weight;
+    public Item(int _profit, int _weight){
+        this.profit = _profit;
+        this.weight = _weight;
     }
 }
 
 public class pr3 {
-
-    public static double fractionalKnapsack(int w, ArrayList<Item> arr) {
-        // Sort items by profit/weight ratio in descending order
-        Collections.sort(arr, new Comparator<Item>() {
-            @Override
-            public int compare(Item o1, Item o2) {
-                double r1 = (double) o1.profit / o1.weight;
-                double r2 = (double) o2.profit / o2.weight;
-                return Double.compare(r2, r1); // Sort in descending order
-            }
-        });
+    public static double fractionalKnapsack(int capacity, ArrayList<Item> array) {
 
         double finalValue = 0.0;
 
-        for (Item item : arr) {
-            if (w >= item.weight) {
-                finalValue += item.profit;
-                w -= item.weight;
+        Collections.sort(array,new Comparator<Item> () {
+            @Override
+            public int compare(Item o1,Item o2){
+                double r1 = o1.profit / o1.weight;
+                double r2 = o2.profit / o2.weight;
+                return Double.compare(r1,r2);
+            }
+        });
+
+        for(Item item : array){
+            if(item.weight <= capacity){
+                finalValue = finalValue + item.profit;
+                capacity = capacity - item.weight;
             } else {
-                finalValue += item.profit * ((double) w / item.weight);
+                finalValue = finalValue + item.profit * ((double) capacity / item.weight);
                 break;
             }
         }
         return finalValue;
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Enter number of items:\n");
-        int n = scanner.nextInt();
+    public static void main(String args[]){
+        ArrayList<Item> array = new ArrayList<Item>();
 
-        ArrayList<Item> arr = new ArrayList<>();
-        
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter profit of item " + (i + 1) + ":\n");
-            int profit = scanner.nextInt();
-            System.out.print("Enter weight of item " + (i + 1) + ":\n");
-            int weight = scanner.nextInt();
-            arr.add(new Item(profit, weight));
-        }
-        
-        System.out.print("Enter capacity of knapsack:\n");
-        int w = scanner.nextInt();
-        
-        double maxValue = fractionalKnapsack(w, arr);
-        System.out.println("Maximum value in knapsack: " + maxValue);
-        
-        scanner.close();
+        array.add(new Item(25,18));
+        array.add(new Item(24,15));
+        array.add(new Item(15,10));
+
+        int capacityOfKnapsack = 20;
+
+        double maxValue = fractionalKnapsack(capacityOfKnapsack,array);
+
+        System.out.println("Max Profit of the Knapsack : " + maxValue);
     }
 }
