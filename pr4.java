bebbf362fbb9b3183da62b2b4b73ec99f8be03ab
Anuj Barve <anujbarve27@gmail.com>
2024-11-06 20:00:37 +0530

    simplified 0/1 knapsack

--- pr4.java
+++ pr4.java
@@ -1,57 +1,53 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class pr4 {
+public class pr4{
+    public static int[] knapsack01(int numOfItem, int[] profits,int[] weights, int capacity){
 
-    public static int[] knapsack01(int n, int[] values, int[] weights, int W) {
-        int[][] dp = new int[n + 1][W + 1];
+        int[][] dp = new int[numOfItem + 1][capacity + 1];
 
-        // Fill dp table
-        for (int i = 0; i <= n; i++) {
-            for (int w = 0; w <= W; w++) {
-                if (i == 0 || w == 0) {
+
+        for(int i = 0; i <= numOfItem; i++){
+            for(int w = 0; w <= capacity; w++){
+                if(i == 0 || w == 0){
                     dp[i][w] = 0;
-                } else if (weights[i - 1] <= w) {
-                    dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1]);
-                } else {
-                    dp[i][w] = dp[i - 1][w];
+                }else if(weights[i-1] <= w){
+                    dp[i][w] = Math.max(dp[i-1][w],dp[i-1][w - weights[i - 1]]+profits[i-1]);
+                }else{
+                    dp[i][w] = dp[i-1][w];
                 }
             }
         }
 
-        // Find selected items
-        List<Integer> selectedItems = new ArrayList<>();
-        int i = n, w = W;
-        while (i > 0 && w > 0) {
-            if (dp[i][w] != dp[i - 1][w]) {
-                selectedItems.add(i - 1);
-                w -= weights[i - 1];
+        List<Integer> selectedItems = new ArrayList<Integer>();
+        int i = numOfItem, w = capacity;
+        while(i > 0 && w > 0){
+            if(dp[i][w] != dp[i-1][w]){
+                selectedItems.add(i-1);
+                w = w - weights[i-1];
             }
             i--;
         }
 
-        // Convert selected items to array and reverse to maintain order
         int[] result = new int[selectedItems.size()];
-        for (int j = 0; j < selectedItems.size(); j++) {
+        for(int j = 0;j < selectedItems.size(); j++){
             result[j] = selectedItems.get(selectedItems.size() - j - 1);
         }
 
-        System.out.println("Maximum value: " + dp[n][W]);
+        System.out.println("Maximum value: " + dp[numOfItem][capacity]);
         System.out.print("Selected items: ");
         for (int item : result) {
             System.out.print(item + " ");
         }
         System.out.println();
-
         return result;
     }
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         int n = 3;
-        int[] values = {60, 100, 120};
-        int[] weights = {10, 20, 30};
-        int W = 50;
+        int[] values = {30, 24, 15};
+        int[] weights = {18, 15, 10};
+        int capacity = 20;
 
-        knapsack01(n, values, weights, W);
+        knapsack01(n, values, weights, capacity);
     }
 }
